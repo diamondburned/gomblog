620a5c2d5517c2d7eff4237c0931589cc7a4298b
diamondburned <diamond@arikawa-hi.me>
2023-01-22 03:18:15 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# gomblog
+
+## Building
+
+```sh
+$ nix-shell
+$ build
+```
+
+## Publishing
+
+Files will be in `public/` after building.
+
+## License
+
+The generator code is licensed under the GNU General Public License version 3.0.
+
+The generated content is licensed under the Creative Commons
+Attribution-NonCommercial-ShareAlike 4.0 International License.
---
+++ shell.nix
@@ -0,0 +1,18 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.mkShell {
+	buildInputs = with pkgs; [
+		cmark
+		gomplate
+		moreutils # for parallel
+		htmlq
+		(pkgs.writeShellScriptBin "serve" ''${pkgs.python3}/bin/python3 -m http.server'')
+	];
+
+	MIME_TYPES_PATH = "${pkgs.mime-types}/etc/mime.types";
+	PROJECT_ROOT = "${builtins.toString ./.}";
+
+	shellHook = ''
+		PATH=${builtins.toString ./.}/tools:$PATH
+	'';
+}
