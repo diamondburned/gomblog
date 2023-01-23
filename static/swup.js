4bd1e9a7895ff3ff485c897488231923fbe7bfc3
diamondburned <diamond@arikawa-hi.me>
2023-01-23 03:43:53 -0800

    Fix Swup pages not running hl.js

--- static/swup.js
+++ static/swup.js
@@ -1,6 +1,7 @@
 import Swup from 'https://cdn.jsdelivr.net/npm/swup@3.0/+esm'
 import Swupscroll from 'https://cdn.jsdelivr.net/npm/@swup/scroll-plugin@2.0/+esm'
 import Swuppreload from 'https://cdn.jsdelivr.net/npm/@swup/preload-plugin@2.0/+esm'
+import hljs from 'https://cdn.jsdelivr.net/npm/highlight.js@11.7.0/+esm'
 
 const swup = new Swup({
 	containers: [ "main" ],
@@ -9,13 +10,16 @@ const swup = new Swup({
 		new Swupscroll(),
 		new Swuppreload(),
 	],
-});
+})
+
+// Re-highlight code blocks after page load.
+swup.on("contentReplaced", () => hljs.highlightAll())
 
 // Preload all our articles.
-const index = document.getElementById('index');
+const index = document.getElementById('index')
 if (index) {
-	const links = index.querySelectorAll('article > a');
+	const links = index.querySelectorAll('article > a')
 	for (const link of links) {
-		swup.preloadPage(link.href);
+		swup.preloadPage(link.href)
 	}
 }
